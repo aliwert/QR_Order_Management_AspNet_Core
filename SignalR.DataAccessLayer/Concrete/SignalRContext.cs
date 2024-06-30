660753ec802081b6ef15517f0c5bbfbf93cf534d
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-30 23:29:07 +0300

    feat:context added

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalR.DataAccessLayer.Concrete
 {
-    internal class SignalRContext
+    public class SignalRContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LRMEISB\\SQLEXPRESS;initial Catalog=SignalRDb;integrated Security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Testimonial>()
+                .HasKey(t => t.TestimonalID);
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
