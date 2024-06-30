8dd98a5c8a88041d477b3e6c0c4fab52c21fdec2
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-30 23:55:40 +0300

    feat: concrete managers

---
+++ SignalR.BusinessLayer/Concret/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutdal;
+
+        public AboutManager(IAboutDal aboutdal)
+        {
+            _aboutdal = aboutdal;
+        }
+
+        public void TAdd(About entity)
+        {
+            _aboutdal.Add(entity);
+        }
+
+        public void TDelete(About entity)
+        {
+            _aboutdal.Delete(entity);
+        }
+
+        public About TGetByID(int id)
+        {
+            return _aboutdal.GetByID(id);
+        }
+
+        public List<About> TGetListtAll()
+        {
+            return _aboutdal.GetListtAll();
+        }
+
+        public void TUpdate(About entity)
+        {
+            _aboutdal.Update(entity);
+        }
+    }
+}
---
+++ SignalR.BusinessLayer/Concret/SocialMediaManager.cs
@@ -0,0 +1,40 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialMediaDal;
+        public void TAdd(SocialMedia entity)
+        {
+            _socialMediaDal.Add(entity);
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socialMediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetByID(int id)
+        {
+            return _socialMediaDal.GetByID(id);
+        }
+
+        public List<SocialMedia> TGetListtAll()
+        {
+            return _socialMediaDal.GetListtAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialMediaDal.Update(entity);
+        }
+    }
+}
