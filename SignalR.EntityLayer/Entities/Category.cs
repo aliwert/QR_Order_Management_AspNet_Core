6732db7e1d956b239015a76125c7856678b982f7
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-30 23:14:33 +0300

    feat: classes added

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalRApi/DAL/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
