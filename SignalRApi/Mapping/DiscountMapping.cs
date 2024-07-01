d78bb597a5bb083b5a89de7a2ca49357e64c0411
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-01 17:28:19 +0300

    feat:mapping

---
+++ SignalRApi/Mapping/DiscountMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.DiscountDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class DiscountMapping: Profile
+    {
+        // Constructor to configure the mappings
+        public DiscountMapping()
+        {
+            // Map between Discount entity and ResultDiscountDto, allowing reverse mapping
+            CreateMap<Discount, ResultDiscountDto>().ReverseMap();
+            // Map between Discount entity and CreateDiscountDto, allowing reverse mapping
+            CreateMap<Discount, CreateDiscountDto>().ReverseMap();
+            // Map between Discount entity and UpdateDiscountDto, allowing reverse mapping
+            CreateMap<Discount, UpdateDiscountDto>().ReverseMap();
+            // Map between Discount entity and GetDiscountDto, allowing reverse mapping
+            CreateMap<Discount, GetDiscountDto>().ReverseMap();
+
+        }
+    }
+}
---
+++ SignalRApi/Mapping/SocialMediaMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class SocialMediaMapping : Profile
+    {
+        public SocialMediaMapping()
+        {
+            CreateMap<SocialMedia, ResultSocialMedia>().ReverseMap();
+            CreateMap<SocialMedia, CreateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia, UpdateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia, GetSocialMediaDto>().ReverseMap();
+
+
+        }
+    }
+}
