6eb29b7aaec2be914dbce675e2107eb9f34e9d15
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-04 22:27:25 +0300

    feat: sidebar component

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke() { return View(); }
+	}
+}
