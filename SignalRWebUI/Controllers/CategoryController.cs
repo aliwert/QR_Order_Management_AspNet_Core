a4128579517231df84241e0aa94da0bf46b4a3ca
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-05 23:55:49 +0300

    feat: now you can add new category

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,32 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-public CategoryController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
-		}
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
-		public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7063/api/Category");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync(); // string method 
-                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>> (jsonData); //list = deserialize,  add & update = serialize
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); //list = deserialize,  add & update = serialize
                 return View(values);
             }
             return View();
         }
+        [HttpGet]
         public IActionResult CreateCategory()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            createCategoryDto.Status = true;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); // 3 parameter => 1 = coming data, 2 = endocing.utf8 , 3 =  
+            var responseMessage = await client.PostAsync("https://localhost:7063/api/Category", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
+
 }
+
