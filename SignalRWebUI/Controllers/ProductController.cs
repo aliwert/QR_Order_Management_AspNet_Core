e5201ef6fa8b4d6eec809128bb5126ffebd6672d
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-07 01:50:37 +0300

    feat: dropdown menu list

--- SignalRWebUI/Controllers/ProductController.cs
+++ SignalRWebUI/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.CategoryDtos;
 using SignalRWebUI.Dtos.ProductDtos;
+using System.Collections.Generic;
 using System.Text;
 
 namespace SignalRWebUI.Controllers
@@ -27,8 +29,19 @@ namespace SignalRWebUI.Controllers
             return View();
         }
         [HttpGet]
-        public IActionResult CreateProduct()
+        public async Task<IActionResult> CreateProduct()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/Category");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value = x.CategoryID.ToString()
+                                            }).ToList();
+            ViewBag.v = values2;
             return View();
         }
         [HttpPost]
