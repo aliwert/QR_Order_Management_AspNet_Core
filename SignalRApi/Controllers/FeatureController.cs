52647b72082587c032d43485e1a7007711f42a9a
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-08 23:51:27 +0300

    feat: features dtos

--- SignalRApi/Controllers/FeatureController.cs
+++ SignalRApi/Controllers/FeatureController.cs
@@ -40,14 +40,14 @@ namespace SignalRApi.Controllers
             });
             return Ok("Discount added");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteFeature(int id)
         {
             var value = _featureService.TGetByID(id);
             _featureService.TDelete(value);
             return Ok("Feature information deleted");
         }
-        [HttpGet("GetFeature")]
+        [HttpGet("{id}")]
         public IActionResult GetFeature(int id)
         {
             var value = _featureService.TGetByID(id);
