9da4c9aa0bdc4fa9fb923e5afc69ab2b296334d7
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-15 22:41:12 +0300

    feat: menu table

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -30,6 +30,8 @@ namespace SignalR.DataAccessLayer.Concrete
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<MoneyCase> MoneyCases { get; set; }
+        public DbSet<MenuTable> MenuTables { get; set; }
+
 
 
     }
