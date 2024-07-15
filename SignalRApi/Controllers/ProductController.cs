cf2170aee46407589df58194f7009c8881799e46
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-15 19:15:47 +0300

    feat: max & min product price add to api controller

--- SignalRApi/Controllers/ProductController.cs
+++ SignalRApi/Controllers/ProductController.cs
@@ -65,6 +65,16 @@ namespace SignalRApi.Controllers
         {
             return Ok(_productService.TProductPriceAvg());
         }
+        [HttpGet("ProductNameByMaxPrice")]
+        public IActionResult ProductNameByMaxPrice()
+        {
+            return Ok(_productService.TProductNameByMaxPrice());
+        }
+        [HttpGet("ProductNameByMinPrice")]
+        public IActionResult ProductNameByMinPrice()
+        {
+            return Ok(_productService.TProductNameByMinPrice());
+        }
         [HttpPost]
         public IActionResult CreateProduct(CreateProductDto createProductDto)
         {
