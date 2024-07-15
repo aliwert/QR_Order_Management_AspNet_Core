be74b0ecec8124980f10ddd1fcf68b45dd70300c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-15 22:02:37 +0300

    feat: money case transactions

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -66,6 +66,10 @@ builder.Services.AddScoped<IOrderDal, EfOrderDal>();
 builder.Services.AddScoped<IOrderDetailService, OrderDetailManager>();
 builder.Services.AddScoped<IOrderDetailDal, EfOrderDetailDal>();
 
+// money case builder
+builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
+builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
