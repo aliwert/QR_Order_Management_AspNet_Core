55fde147299b68cf22d7fec840cec0ec6bf80548
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-17 00:21:18 +0300

    feat: about consume

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultAboutComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/About");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+
+            return View(values);
         }
     }
 }
