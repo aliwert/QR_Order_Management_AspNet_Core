4519236152ff42de7b74a3bea38a84bd92d44d77
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-16 22:53:23 +0300

    feat: slider dto

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/Sliders");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync(); // string method 
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData); //list = deserialize,  add & update = serialize
+            return View(values);
         }
     }
 }
