0a1afffa250e59513fd9fe2befe15644e774f239
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-16 23:19:14 +0300

    feat: main titles & descriptions

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -7,17 +7,20 @@ namespace SignalRWebUI.ViewComponents.DefaultComponents
     public class _DefaultSliderComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
+
         public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
-        public async Task<IViewComponentResult> Invoke()
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7063/api/Sliders");
 
-            var jsonData = await responseMessage.Content.ReadAsStringAsync(); // string method 
-            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData); //list = deserialize,  add & update = serialize
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+
             return View(values);
         }
     }
