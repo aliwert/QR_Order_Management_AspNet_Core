6c9ac96b9c3147b589c080f920c5f9f660095f21
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-18 01:47:20 +0300

    feat: designe basket page

---
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BasketDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BasketsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BasketsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/Basket");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // string method 
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData); //list = deserialize,  add & update = serialize
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
