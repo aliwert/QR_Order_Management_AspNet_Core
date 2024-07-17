19b1f865dff5abe6faa20b53e2a169e07b93e66c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-18 01:29:01 +0300

    feat: basket api

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -78,7 +78,9 @@ builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 builder.Services.AddScoped<ISliderService, SliderManager>();
 builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 
-
+// basket builder
+builder.Services.AddScoped<IBasketService, BasketManager>();
+builder.Services.AddScoped<IBasketDal, EfBasketDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
