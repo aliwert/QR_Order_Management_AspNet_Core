508c868b58a0337082fad6badb5642f3f4f37bed
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-18 23:23:05 +0300

    feat: special product name api

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalRApi.Models;
 
 namespace SignalRApi.Controllers
 {
@@ -19,6 +22,23 @@ namespace SignalRApi.Controllers
         {
             var values = _basketService.TGetBasketByMenuTableNumber(id);
             return Ok(values);
+        }
+        [HttpGet("BasketListByMenuTableWithProductName")]
+        public IActionResult BasketListByMenuTableWithProductName(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Include(x => x.Product).Where(y => y.MenuTableID == id).Select(z => new ResultBasketListWithProducts
+            {
+                BasketID = z.BasketID,
+                Count = z.Count,
+                MenuTableID = z.MenuTableID,
+                Price = z.Price,
+                ProductID = z.ProductID,
+                ProductName = z.Product.ProductName,
+                TotalPrice = z.TotalPrice,
+            }).ToList();
+            return Ok(values);
+
         }
     }
 }
