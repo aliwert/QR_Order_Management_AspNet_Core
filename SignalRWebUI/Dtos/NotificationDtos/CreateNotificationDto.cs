6fd7cf2e611c61a1dcf1e61da1a3f40725a506a4
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-20 23:27:37 +0300

    feat: create notification page

--- SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
+++ SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -11,7 +11,6 @@ namespace SignalRWebUI.Dtos.NotificationDtos
 		public string Type { get; set; }
 		public string Icon { get; set; }
 		public string Description { get; set; }
-		public DateTime Date { get; set; }
 		public bool Status { get; set; }
 	}
 }
