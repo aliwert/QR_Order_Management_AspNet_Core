1f3111b635998503b3802e399f3a87eacb53989e
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-20 22:19:04 +0300

    feat: notification api

--- SignalRApi/Controllers/NotificationsController.cs
+++ SignalRApi/Controllers/NotificationsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -29,6 +31,47 @@ namespace SignalRApi.Controllers
         {
             return Ok(_notificationService.TGetAllNotificationByFalse());
         }
-
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                Description = createNotificationDto.Description,
+                Icon = createNotificationDto.Icon,
+                Status = false,
+                Type = createNotificationDto.Type,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Insertion done successfully");
+        }
+        [HttpDelete]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Notification deleted");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                NotificationId = updateNotificationDto.NotificationId,
+                Description = updateNotificationDto.Description,
+                Icon = updateNotificationDto.Icon,
+                Status = updateNotificationDto.Status,
+                Type = updateNotificationDto.Type,
+                Date = updateNotificationDto.Date,
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Notification updated.");
+        }
     }
 }
