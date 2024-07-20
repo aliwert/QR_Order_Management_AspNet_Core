322203f069ac9d3a063e7f09c3e349683cdd8fca
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-21 01:12:45 +0300

    feat: notification business layer(service & manager)

--- SignalR.BusinessLayer/Abstract/INotificationService.cs
+++ SignalR.BusinessLayer/Abstract/INotificationService.cs
@@ -11,5 +11,7 @@ namespace SignalR.BusinessLayer.Abstract
     {
         public int TNotificationCountByStatusFalse();
         List<Notification> TGetAllNotificationByFalse();
+        void TNotificationStatusChangeToTrue(int id);
+        void TNotificationStatusChangeToFalse(int id);
     }
 }
