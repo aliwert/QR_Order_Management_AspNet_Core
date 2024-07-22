69a877ddae8132a5dc6c65d9e01d3133d4c60525
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-23 02:49:26 +0300

    chore: bookings revise

--- SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
+++ SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
@@ -10,6 +10,7 @@ namespace SignalR.DtoLayer.BookingDto
     {
         public string Name { get; set; }
         public string Phone { get; set; }
+        public string Description { get; set; }
         public string Mail { get; set; }
         public int PersonCount { get; set; }
         public DateTime Date { get; set; }
--- SignalRWebUI/Dtos/BookingDtos/CreateBookingDto.cs
+++ SignalRWebUI/Dtos/BookingDtos/CreateBookingDto.cs
@@ -9,6 +9,7 @@ namespace SignalRWebUI.Dtos.BookingDtos
     public class CreateBookingDto
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Phone { get; set; }
         public string Mail { get; set; }
         public int PersonCount { get; set; }
