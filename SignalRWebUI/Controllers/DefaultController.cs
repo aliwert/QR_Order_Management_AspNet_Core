472929860dbd7635e71e070a115e634f5c618119
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-25 01:17:52 +0300

    feat: ui message section

--- SignalRWebUI/Controllers/DefaultController.cs
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.MessageDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,9 +23,18 @@ namespace SignalRWebUI.Controllers
             return PartialView();
         }
         [HttpPost]
-        public IActionResult SendMessage()
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
         {
-            return RedirectToAction("Index");
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMessageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); // 3 parameter => 1 = coming data, 2 = endocing.utf8 , 3 =  
+            var responseMessage = await client.PostAsync("https://localhost:7063/api/Message", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
