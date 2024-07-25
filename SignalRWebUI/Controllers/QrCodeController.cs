cb8a40b988d276009db5e2cfbb20ffc3b6587bb5
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-26 02:10:40 +0300

    feat: qrcode controller

---
+++ SignalRWebUI/Controllers/QrCodeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+namespace SignalRWebUI.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        public IActionResult Index(string value)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator createQRCode = new QRCodeGenerator();
+                QRCodeGenerator.QRCode squareCode = createQRCode.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                using (Bitmap image = squareCode.GetGraphic(10))
+                {
+                    image.Save(ms, ImageFormat.Png);
+                    ViewBag.QrCodeImage = "data:image/png;base64" + Convert.ToBase64String(ms.ToArray());
+                }
+
+            }
+            return View();
+        }
+    }
+}
