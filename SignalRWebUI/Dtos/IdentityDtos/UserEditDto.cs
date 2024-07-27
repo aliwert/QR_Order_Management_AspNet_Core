7e7e94feba8ff522ca1d131caae5db09c7684094
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-27 15:58:31 +0300

    feat: settings index

--- SignalRWebUI/Dtos/IdentityDtos/UserEditDto.cs
+++ SignalRWebUI/Dtos/IdentityDtos/UserEditDto.cs
@@ -7,6 +7,8 @@
         public string Surname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
 
     }
 }
